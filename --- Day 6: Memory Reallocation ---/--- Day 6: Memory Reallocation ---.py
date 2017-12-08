5eb33104b7bf651d08e671467f03e96ee6f00b7b
Dirk-Jan van Helmond <djvanhelmond@gmail.com>
2017-12-08 17:45:09 +0100

    day 6

---
+++ --- Day 6: Memory Reallocation ---/--- Day 6: Memory Reallocation ---.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+
+#INPUT = [ 0, 2, 7, 0 ]
+INPUT = [ 0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11 ]
+
+class Memory():
+    def __init__(self, configuration):
+        self.cycles = 0
+        self.looplength = 0
+        self.banks = {}
+        self.seen = {}
+        for i in range(len(configuration)):
+            self.banks[i] = configuration[i]
+
+    def findMostBlocks(self):
+        highest = 0
+        for i in self.banks.keys():
+            if self.banks[i] > self.banks[highest]:
+                highest = i
+        return highest
+
+    def redistributeBlocks(self, bankId):
+        blocks = self.banks[bankId]
+        self.banks[bankId] = 0
+        for i in range(1, blocks + 1):
+            self.banks[(bankId + i) % len(self.banks)] = self.banks[(bankId + i) % len(self.banks)] + 1
+
+    def cycle(self):
+        while not list(self.banks.values()) in self.seen.values():
+            self.cycles += 1
+            self.seen[self.cycles] = list(self.banks.values())
+            self.redistributeBlocks(self.findMostBlocks())
+        for k, v in self.seen.iteritems():
+            if v == list(self.banks.values()):
+                self.looplength = self.cycles - k + 1
+
+
+mem = Memory(INPUT)
+mem.cycle()
+print("Star 1: %i" % len(mem.seen))
+print("Star 2: %i" % mem.looplength)
