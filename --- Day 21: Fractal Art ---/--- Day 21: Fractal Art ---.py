0f8dffd0e69d214d83cc21bd9c84d6ec273d9cbf
Dirk-Jan van Helmond <djvanhelmond@gmail.com>
2017-12-21 20:15:36 +0100

    day 18 star 2 - 66 LOC

--- --- Day 21: Fractal Art ---/--- Day 21: Fractal Art ---.py
+++ --- Day 21: Fractal Art ---/--- Day 21: Fractal Art ---.py
@@ -4,7 +4,7 @@ import math
 class artGenerator():
     def __init__(self, rules):
         self.art = [".#.", "..#", "###" ]
-        self.enhancementRules = self.__augmentEnhancementRules(rules)
+        self.enhanceRules = self.__augmentEnhancementRules(rules)
 
     def __flipPattern(self, inPattern):
         return "/".join([ element[::-1] for element in inPattern.split("/") ])
@@ -33,40 +33,32 @@ class artGenerator():
                 rulebase[rotPattern] = outPattern
         return rulebase
 
-    def __cutArtinSquares(self, art, size):
+    def __cutArtinSquares(self, art):
+        if (len(self.art) % 2 == 0): size = 2
+        else: size = 3
         squares = []
         for i in range(0, len(art), size):
             for j in range(0, len(art[i]), size):
-                squares.append( "/".join([ art[k][j:j+size] for k in range(i, i+size) ]))
+                squares.append( "/".join([ art[k][j:j + size] for k in range(i, i + size) ]))
         return squares
 
-    def __recombineSquares(self, squares, size):
+    def __recombineSquares(self, squares):
         artSize = int(math.sqrt(len(squares)))
         squares = [ square.split("/") for square in squares ]
         newArt = []
-        for i in range(0, artSize):
-            for j in range(size+1):
-                line = []
-                for k in range(artSize):
-                    line.append(squares[(i*artSize)+k][j])
-                newArt.append("".join(line))
+        for i in range(artSize):
+            for j in range(len(squares[i])):
+                newArt.append("".join([ squares[(i*artSize)+k][j] for k in range(artSize) ]))
         return newArt
 
     def iterate(self):
-        if (len(self.art) % 2 == 0): size = 2
-        else: size = 3
-        enhancedSquares = []
-        for square in self.__cutArtinSquares(self.art, size):
-            enhancedSquares.append(self.enhancementRules[square])
-        self.art = self.__recombineSquares(enhancedSquares, size)
+        self.art = self.__recombineSquares([ self.enhanceRules[square] for square in self.__cutArtinSquares(self.art) ])
 
     def countOn(self):
         return sum([ i.count("#") for i in self.art ])
 
 
-with open("./input.txt") as f:
-    INPUT = f.readlines()
-
+with open("./input.txt") as f: INPUT = f.readlines()
 art = artGenerator(INPUT)
 for i in range(18):
     if i == 5: print("Star 1: %i" % art.countOn())
